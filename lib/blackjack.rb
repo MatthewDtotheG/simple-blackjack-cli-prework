815cceb188a1c3a0d526a16787c4d311984fc11f
Matthew Garrett <mdgarrett1984@gmail.com>
2018-01-02 03:39:46 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -63,9 +63,10 @@ end
 def runner
   # code runner here
   welcome
-  initial_round
-  hit?
+  card_total = initial_round
   until card_total > 21
-    end_game
+    card_total = hit?(card_total)
+    display_card_total(card_total)
   end
+  end_game(card_total)
 end
