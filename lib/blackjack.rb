3839f7a0daf1e5755abd075f9d6b46b85dbe01f3
Matthew Garrett <mdgarrett1984@gmail.com>
2018-01-02 03:38:35 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -37,7 +37,18 @@ def initial_round
 end
 
 def hit?(card_total)
-
+    prompt_user
+    input = get_user_input
+    until input == 'h' || input == 's'
+      invalid_command
+      prompt_user
+      input = get_user_input
+    end
+    if input == 'h'
+      card_total += deal_card
+    elsif input == 's'
+      card_total
+  end
 end
 
 def invalid_command
@@ -51,4 +62,10 @@ end
 
 def runner
   # code runner here
+  welcome
+  initial_round
+  hit?
+  until card_total > 21
+    end_game
+  end
 end
